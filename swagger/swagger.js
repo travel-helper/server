53bf5363ef31cab828b4a83c8bf76cd3e7e5fb1f
oeyh98 <skjrjr123@naver.com>
2022-09-25 17:20:50 +0900

    initial dir_empty

---
+++ swagger/swagger.js
@@ -0,0 +1,22 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggereJsdoc = require("swagger-jsdoc");
+
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Travle_Helper API",
+      version: "1.0.0",
+      description: "여행떠나조 Travle_Helper Swagger API",
+    },
+    server: [
+      {
+        url: "http://localhost:3000/docs", // url
+        description: "Local server", // name
+      },
+    ],
+  },
+  apis: ["./routers/*.js", "./controllers/*.js", "./swagger/swaggerDoc.js"],
+};
+const specs = swaggereJsdoc(options);
+module.exports = { swaggerUi, specs };
