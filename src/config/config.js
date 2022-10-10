243ff7e0aea61d7c21d27d8c59c0e3d9866982ef
Lcheck <znfhdl789@naver.com>
2022-10-11 01:03:30 +0900

    Fix config.js 수정

--- src/config/config.js
+++ src/config/config.js
@@ -2,26 +2,26 @@ require("dotenv").config();
 const env = process.env;
 
 const development = {
-  username: env.MYSQL_USERNAME,
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DATABASE,
-  host: env.MYSQL_HOST,
+  username: env.DB_USER,
+  password: env.DB_PASS,
+  database: "Travel",
+  host: env.DB_HOST,
   dialect: "mysql",
 };
 
 const production = {
-  username: env.MYSQL_USERNAME,
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DATABASE,
-  host: env.MYSQL_HOST,
+  username: env.DB_USER,
+  password: env.DB_PASS,
+  database: "TravelP",
+  host: env.DB_HOST,
   dialect: "mysql",
 };
 
 const test = {
-  username: env.MYSQL_USERNAME,
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DATABASE_TEST,
-  host: env.MYSQL_HOST,
+  username: env.DB_USER,
+  password: env.DB_PASS,
+  database: "test",
+  host: env.DB_HOST,
   dialect: "mysql",
 };
 
