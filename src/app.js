61ee0991afb046723b80c47c374583278dab4a27
Lcheck <znfhdl789@naver.com>
2022-10-24 15:33:42 +0900

    Fix app.js 누락된 부분 복구

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const dotenv = requite("dotenv");
+const {sequelize} = require('./models');
+const postRouter = require('./routers/post.router')
 //const morgan = require("morgan");
 //const cookieParser = require("cookie-parsers");
 
@@ -16,7 +18,37 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 const options = require("../swagger/swaggerDoc");
 
+
+
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.use(helmet());
-app.use(cors()); //허용 도메인 설정
+app.use(cors(
+    // {
+    // origin:'*',
+    // credentials:true,
+    // }
+    )); //허용 도메인 설정
 //app.use(morgan("tiny"));
+app.use('/post', postRouter);
+
+sequelize.sync({ force: false })
+   .then(() => {
+      console.log('데이터베이스 연결됨.');
+   }).catch((err) => {
+      console.error(err);
+   });
+//mysql와 sequelize 동기화
+
+app.get('/',(req,res)=>{
+    res.send('백엔드 서버 실행중');
+    })//실행확인용
+
+
+
+
+app.listen(443,()=>{
+
+
+console.log('서버실행중')});
