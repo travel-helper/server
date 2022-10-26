935dc4bf65749bc57f3680d8f32974745f0a5a5f
oeyh98 <skjrjr123@naver.com>
2022-10-26 19:38:20 +0900

    Fix 파일 구조 정정

--- src/models/index.js
+++ src/models/index.js
@@ -1,39 +1,46 @@
 "use strict";
 
+const fs = require("fs");
+const path = require("path");
 const Sequelize = require("sequelize");
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config")[env];
+const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
-//config의 정보대로 시퀄라이즈와 mysql을 연결함
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 
-db.Comment = require("./comment")(sequelize, Sequelize);
-db.Hashtag = require("./hashtag")(sequelize, Sequelize);
-db.Image = require("./image")(sequelize, Sequelize);
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-
-db.Post = require('./post')(sequelize,Sequelize);
-db.User = require('./user')(sequelize,Sequelize);
-db.Comment = require('./comment')(sequelize,Sequelize);
-db.Hashtag = require('./hashtag')(sequelize,Sequelize);
-db.Image = require('./image')(sequelize,Sequelize);
-
-//모델 불러오기, 생성된 sequelize객체를 전달함, 모델에서는 해당 객체에 table 생성
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
-//모델 관계 설정
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = db;
