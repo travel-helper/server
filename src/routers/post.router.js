5a9ececccda4d0ba734b1720c29e8b9df6553bdd
oeyh98 <skjrjr123@naver.com>
2022-10-31 19:52:36 +0900

    feat passport

--- src/routers/post.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const postController = require("../controllers/postController");
-
-const router = express.Router();
-
-router.get("/", postController.loadPosts);
-
-module.exports = router;
