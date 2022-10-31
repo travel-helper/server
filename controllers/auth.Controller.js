b490b5b21539a7e5138f62692ef322645089afb6
oeyh98 <skjrjr123@naver.com>
2022-10-31 19:42:36 +0900

    Feat passport

---
+++ controllers/auth.Controller.js
@@ -0,0 +1,10 @@
+import passport from "passport";
+
+const authService = require("../services/auth.Service");
+
+const baseResponse = require("../utilities/baseResponseStatus");
+const { errResponse, response } = require("../utilities/response");
+
+export async function signup(req, res) {
+  passport;
+}
--- controllers/user.Controller.js
+++ controllers/user.Controller.js
@@ -1,4 +1,4 @@
-const userService = require("../services/userService");
+const userService = require("../services/user.Service");
 
 const baseResponse = require("../utilities/baseResponseStatus");
 const { errResponse, response } = require("../utilities/response");
@@ -6,7 +6,7 @@ const regexDate = new RegExp(
   /(^(19|20)\d{2})(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])$/
 ); // YYYYMMDD 확인 정규표현식
 
-exports.loadUsers = async function (req, res) {
+exports.loadUsers = async function (req, res, next) {
   const result = await postUser.loadUser();
   return res.status(200).json(result);
 };
