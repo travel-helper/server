094ee2e9dca2a85ea422cdbd62cb4d39617b3bcd
Lcheck <znfhdl789@naver.com>
2022-11-14 10:55:19 +0900

    Fix app.use 코드 순서 및 urlencoded 값 변경

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const postRouter = require("./routers/post.router");
 const userRouter = require("./routers/user.router");
 const cookieParser = require("cookie-parser");
 const passport = require("passport");
-const session = require("express-session")
+const session = require("express-session");
 const passportConfig = require("./passport");
 
 dotenv.config();
@@ -25,7 +25,7 @@ const { compareSync } = require("bcrypt");
 //클라이언트에서 보내준 데이터를 json으로 파싱해서 req.body에 전송
 app.use(express.json());
 //로그인, 회원가입 등 form태그에서 submit하여 전달할 때 form 파싱
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 // app.use(compression());
 app.use(helmet());
 app.use(
@@ -37,9 +37,6 @@ app.use(
 ); //허용 도메인 설정
 app.use(cookieParser(process.env.COOKIE_SECRET)); //cookieparser에 비밀키 설정
 app.use(morgan("dev")); //개발모드로 로깅
-
-app.use(passport.initialize()); //passport 초기화
-app.use(passport.session()); //페이지 내에서 영구 로그인 설정 (변경 요)
 
 sequelize
   .sync({ force: false })
@@ -62,7 +59,7 @@ const {
 } = process.env;
 
 // app.use("/post", postRouter);
-// app.use("/user", userRouter);
+app.use("/user", userRouter);
 
 
 app.use(cookieParser('travelH')); // 키 코드로 쿠기 생성 -> 추후 dotenv로 키값을 옮길 예정
@@ -74,6 +71,9 @@ resave:false,
 secret:'travelH',
 
 }));
+
+app.use(passport.initialize()); //passport 초기화
+app.use(passport.session()); //페이지 내에서 영구 로그인 설정 (변경 요)
 
 passportConfig(); //패스포트 설정 실행
 
