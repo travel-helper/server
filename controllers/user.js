0d3300353ae768477d8c73c7f58d21d072735b9c
Lcheck <znfhdl789@naver.com>
2022-11-27 17:10:09 +0900

    Fix logout 컨트롤러 수정

--- controllers/user.js
+++ controllers/user.js
@@ -26,6 +26,9 @@ exports.loadUsers = async function (req, res, next) {
 exports.login = async function (req, res, next) {
   // 패스포트를 활용한 로그인 컨트롤러(서비스는 패스포트 내에 존재함)
 
+  // passport.authenticate() 구현 부분
+  // authenticate(strategy: string | string[] | Strategy, callback?: (...args: any[]) => any): AuthenticateRet;
+  // authenticate(strategy: string | string[] | Strategy, options: AuthenticateOptions, callback?: (...args: any[]) => any): AuthenticateRet;
   // passport.authenticate("jwt", { session: false })(req, res, next);
 
   passport.authenticate("local", (err, user, info) => {
@@ -53,6 +56,29 @@ exports.login = async function (req, res, next) {
   })(req, res, next);
 };
 
+// exports.login = async function (req, res, next) {
+//   // 패스포트를 활용한 로그인 컨트롤러(서비스는 패스포트 내에 존재함)
+
+//   passport.authenticate("jwt", { session: false }, (err, user, info) => {
+//     console.log(user);
+
+//     if (err) {
+//       console.error(err);
+//       next(err);
+//     }
+
+//     if (info) {
+//       return res.status(401).send(info.reason);
+//     }
+
+//     const fullUser = userService.fullUserWithoutPassword(user);
+//     // console.log(fullUser.dataValues);
+//     const token = jwt.sign(fullUser.dataValues, "jwt-secret-key");
+//     return res.status(200).json(token);
+//     // return res.status(200).json(fullUser);
+//   })(req, res, next);
+// };
+
 exports.signup = async function (req, res, next) {
   // POST /user/
   console.log("회원가입요청");
@@ -73,10 +99,6 @@ exports.signup = async function (req, res, next) {
 
 exports.logout = async function (req, res, next) {
   //로그아웃
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    return res.send("ok");
-  });
+
+  return res.send("ok");
 };
